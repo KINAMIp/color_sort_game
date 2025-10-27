2604220654cf12c1725fc3fedf143cc252d3aac8
KINAMIp <154564499+KINAMIp@users.noreply.github.com>
2025-10-27 04:39:56 +0300

    Handle taps manually without HasTappables

--- lib/src/game/crayon_game.dart
+++ lib/src/game/crayon_game.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 
 import '../data/level_model.dart';
 import '../services/audio_service.dart';
@@ -11,7 +12,7 @@ import '../state/app_state.dart';
 import 'components/tube_component.dart';
 import 'systems/pour_system.dart';
 
-class CrayonGame extends FlameGame with HasTappableComponents {
+class CrayonGame extends FlameGame with TapDetector {
   CrayonGame({
     required this.level,
     required this.appState,
@@ -39,6 +40,18 @@ class CrayonGame extends FlameGame with HasTappableComponents {
 
   List<List<Color>> get _currentStacks =>
       tubes.map((tube) => tube.segments.map((segment) => segment.color).toList()).toList();
+
+  @override
+  void onTapDown(TapDownInfo info) {
+    super.onTapDown(info);
+    final tapPosition = info.eventPosition.game;
+    for (final tube in tubes) {
+      if (tube.containsPoint(tapPosition)) {
+        tube.handleTap();
+        break;
+      }
+    }
+  }
 
   @override
   Future<void> onLoad() async {
