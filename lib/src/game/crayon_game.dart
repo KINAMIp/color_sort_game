813c7dacb8e947d55b4a445a9388f3d4498c573a
KINAMIp <154564499+KINAMIp@users.noreply.github.com>
2025-10-27 04:23:05 +0300

    Fix tappable mixin for latest Flame

--- lib/src/game/crayon_game.dart
+++ lib/src/game/crayon_game.dart
@@ -11,7 +11,7 @@ import '../state/app_state.dart';
 import 'components/tube_component.dart';
 import 'systems/pour_system.dart';
 
-class CrayonGame extends FlameGame with HasTappables {
+class CrayonGame extends FlameGame with HasTappableComponents {
   CrayonGame({
     required this.level,
     required this.appState,
